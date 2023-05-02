3ae00b85e50def480202a92ffabdb490f6b3f986
Bloodov Pavel <kasper6024@gmail.com>
2023-05-02 18:12:47 +0300

    move code to reusable QuerySet method.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.db.models import Count
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
+        return posts_at_year
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        tag_at_posts = self.annotate(tags_count=Count('posts')).order_by('-tags_count')
+        return tag_at_posts
 
 
 class Post(models.Model):
@@ -24,6 +39,7 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -39,6 +55,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
